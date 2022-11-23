7f8f06c5775b5204d2c49d2f0bc8825e71feb867
QASchoolUSA <qaschoolusa@gmail.com>
2022-11-23 03:00:37 -0500

    Added new changes

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,7 +4,7 @@ import path, { dirname } from "path";
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const files = fs.readdirSync(__dirname + "/videos");
+const files = fs.readdirSync(path.join(__dirname, '..', 'videos/'));
 console.log(files);
 const pass:any = process.env.PASS;
 const email:any = process.env.EMAIL;
